5f744cbfcd67d8b015a99c46b183aa5c2ab4cce8
chick_0 <chick_0@kakao.com>
2024-06-22 07:17:13 +0900

    폰트 설정 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import "$lib/css/base.css"
     import LoadingController from "$lib/controller/LoadingController.svelte"
     import LockController from "$lib/controller/LockController.svelte"
 </script>
