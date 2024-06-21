9c45fb9735828e59a024261d26c74ebdae6c0258
chick_0 <chick_0@kakao.com>
2024-06-22 08:15:32 +0900

    hit/stand 기능 추가

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte"
     import { Computer, GameStatus, Player, resetGame } from "$lib/store"
     import { getHandScore, getLimitedHandScore } from "$lib/score"
+    import { hitCard, actionAuto, getWinner } from "$lib/action"
 
     import SpinnerWrapper from "$lib/layout/SpinnerWrapper.svelte"
     import Spinner from "$lib/props/Spinner.svelte"
@@ -14,6 +15,61 @@
         isMounted = true
         resetGame()
     })
+
+    function computerAction() {
+        const card = actionAuto($Computer)
+
+        if (card != null) {
+            Computer.set([...$Computer, card])
+            return true
+        }
+
+        return false
+    }
+
+    function playerHit() {
+        if ($GameStatus != "playing") {
+            alert("게임이 종료되었습니다")
+            return
+        }
+
+        const card = hitCard()
+
+        if (card == null) {
+            alert("덱에 남은 카드가 없습니다!")
+            return
+        }
+
+        Player.set([...$Player, card])
+        computerAction()
+
+        if (getHandScore($Player) > 21) {
+            alert("bust")
+            calcGameResult()
+        }
+    }
+
+    function playerStand() {
+        if ($GameStatus != "playing") {
+            alert("게임이 종료되었습니다")
+            return
+        }
+
+        calcGameResult()
+    }
+
+    function calcGameResult() {
+        while (true) {
+            if (!computerAction()) {
+                break
+            }
+        }
+
+        GameStatus.set("result")
+
+        const winner = getWinner($Computer, $Player)
+        alert(winner)
+    }
 </script>
 
 {#if !isMounted}
@@ -21,32 +77,40 @@
         <Spinner />
     </SpinnerWrapper>
 {:else}
-    <button on:click={() => resetGame()}>Reset</button>
-    <button on:click={() => GameStatus.set("result")}>Result</button>
+    <div>
+        <button on:click={() => resetGame()}>Reset</button>
+    </div>
 
-    <h1>Computer</h1>
-    {#if $GameStatus == "result"}
-        <p>{getHandScore($Computer)}</p>
-    {:else}
-        <p>{getLimitedHandScore($Computer)}</p>
-    {/if}
+    <div>
+        <h1 class="name">Computer</h1>
+        {#if $GameStatus == "result"}
+            <p class="score">{getHandScore($Computer)}</p>
+        {:else}
+            <p class="score">{getLimitedHandScore($Computer)}</p>
+        {/if}
 
-    <CardWrapper>
-        {#each $Computer as card, i}
-            {#if i == 0 || $GameStatus == "result"}
+        <CardWrapper>
+            {#each $Computer as card, i}
+                {#if i == 0 || $GameStatus == "result"}
+                    <Card {card} />
+                {:else}
+                    <Card card="Back" />
+                {/if}
+            {/each}
+        </CardWrapper>
+    </div>
+
+    <div>
+        <h1 class="name">Player</h1>
+        <p class="score">{getHandScore($Player)}</p>
+
+        <CardWrapper>
+            {#each $Player as card}
                 <Card {card} />
-            {:else}
-                <Card card="Back" />
-            {/if}
-        {/each}
-    </CardWrapper>
+            {/each}
+        </CardWrapper>
 
-    <h1>Player</h1>
-    <p>{getHandScore($Player)}</p>
-
-    <CardWrapper>
-        {#each $Player as card}
-            <Card {card} />
-        {/each}
-    </CardWrapper>
+        <button on:click={playerHit}>Hit</button>
+        <button on:click={playerStand}>Stand</button>
+    </div>
 {/if}
