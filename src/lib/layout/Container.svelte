abfbebfe8a1e593c79f2f736b79b5fd6433333c6
chick_0 <chick_0@kakao.com>
2024-06-22 08:14:32 +0900

    컨테이너 추가

---
+++ src/lib/layout/Container.svelte
@@ -0,0 +1,27 @@
+<div class="container">
+    <slot />
+</div>
+
+<style>
+    .container {
+        --container: 250px;
+
+        padding-top: 20px;
+        padding-bottom: 20px;
+
+        padding-left: var(--container);
+        padding-right: var(--container);
+    }
+
+    @media (max-width: 768px) {
+        .container {
+            --container: 10px;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 1200px) {
+        .container {
+            --container: 50px;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
     import "$lib/css/base.css"
+    import Container from "$lib/layout/Container.svelte"
     import LoadingController from "$lib/controller/LoadingController.svelte"
     import LockController from "$lib/controller/LockController.svelte"
 </script>
 
-<slot />
+<Container>
+    <slot />
+</Container>
 
 <LoadingController />
 <LockController />
