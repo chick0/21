3da9dd2acb5be8b6a849c54758d5d826d44f6ddd
chick_0 <chick_0@kakao.com>
2024-06-24 22:27:38 +0900

    확대/축소 제한 추가

---
+++ src/lib/controller/ScalableLockController.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onDestroy, onMount } from "svelte"
+
+    /** @type {string | null} */
+    let restoreContent = null
+
+    onMount(() => {
+        /** @type {HTMLMetaElement | null} */
+        const viewport = document.querySelector("meta[name=viewport]")
+
+        if (viewport != null) {
+            restoreContent = viewport.content
+            viewport.content = restoreContent + ", user-scalable=no"
+        }
+    })
+
+    onDestroy(() => {
+        if (restoreContent != null) {
+            /** @type {HTMLMetaElement | null} */
+            const viewport = document.querySelector("meta[name=viewport]")
+
+            if (viewport != null) {
+                viewport.content = restoreContent
+            }
+        }
+    })
+</script>
--- src/routes/game/+layout.svelte
+++ src/routes/game/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
+    import ScalableLockController from "$lib/controller/ScalableLockController.svelte"
     import MetaController from "$lib/controller/MetaController.svelte"
     import BackButton from "$lib/props/BackButton.svelte"
     import Container from "$lib/layout/Container.svelte"
 </script>
+
+<ScalableLockController />
 
 <MetaController title="21!" description="21 게임을 플래이합니다." />
 <BackButton />
